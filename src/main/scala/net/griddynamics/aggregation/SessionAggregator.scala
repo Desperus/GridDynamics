8296c76d61430c9177e4beac407753f31ddec580
Aleksandr_Meterko <aleksandr_meterko@epam.com>
2018-08-24 22:32:42 +0400

    Implemented session id calculation

--- src/main/scala/net/griddynamics/aggregation/SessionAggregator.scala
+++ src/main/scala/net/griddynamics/aggregation/SessionAggregator.scala
@@ -11,6 +11,8 @@ import org.apache.spark.sql.functions._
   */
 object SessionAggregator {
 
+  private val DefaultTimeoutSeconds = 5 * 60
+
   def enrich(spark: SparkSession, filePath: String): Unit = {
     val events = spark.read
       .format("csv")
@@ -18,15 +20,23 @@ object SessionAggregator {
       .option("mode", "DROPMALFORMED")
       .load(filePath)
 
-    val seconds = 5 * 60
-    val window = Window.partitionBy("category").orderBy("eventTimeCast").rangeBetween(-seconds, Window.currentRow)
+    val rowWindow = Window.partitionBy("category")
+      .orderBy("eventTimeCast")
+    val timeWindow = rowWindow.rangeBetween(-DefaultTimeoutSeconds, Window.currentRow)
+    val sessionIdWindow = Window.orderBy("category", "eventTimeCast")
+      .rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
-    events
+    val result = events
       .withColumn("eventTimeCast", unix_timestamp(col("eventTime")))
-      .withColumn("sessionStartTime", first("eventTime").over(window))
-      .withColumn("sessionEndTime", last("eventTime").over(window))
-      .show(30)
+      .withColumn("prevTimestamp", lag("eventTimeCast", 1).over(rowWindow))
+      .withColumn("isNewSession", when(lag("eventTime", 1).over(rowWindow).isNull, 1).otherwise(0))
+      .withColumn("sessionId", sum("isNewSession").over(sessionIdWindow))
+      .withColumn("sessionStartTime", first("eventTime").over(timeWindow))
+      .withColumn("sessionEndTime", last("eventTime").over(timeWindow))
+      .select(col("category"), col("eventTime"), col("sessionId"))
 
+    result.show(30)
+    println(result.count())
   }
 
 }
